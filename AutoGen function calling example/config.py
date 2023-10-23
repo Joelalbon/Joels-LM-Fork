b0ea86e1c4ab0aa397d71751ea532466a3fe5a39
Christopher Lege <christopher.lege@googlemail.com>
2023-10-23 21:27:14 +0200

    Update config.py
    Make config more "out of the box" for LM Studio users running only one machine.

--- AutoGen function calling example/config.py
+++ AutoGen function calling example/config.py
@@ -6,15 +6,19 @@ open_ai_config_list = autogen.config_list_from_json(
         "model": ["gpt-3.5-turbo"],
     },
 )
-
+'''
+Change localhost to an IP of the machine running LM Studio if on local network.
+'''
 coders = [{
     "api_type": "open_ai",
-    "api_base": "http://192.168.123.34:1234/v1",
+    "api_base": "http://localhost:1234/v1",
     "api_key": "NULL",
 }]
-
+'''
+Change localhost to an IP of the machine running LM Studio if on local network.
+'''
 assistants = [{
     "api_type": "open_ai",
-    "api_base": "http://192.168.123.59:1234/v1",
+    "api_base": "http://localhost:1234/v1",
     "api_key": "NULL",
 }]
